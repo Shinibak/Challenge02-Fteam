5e17d9ec751cafef4f854ded6452d8b03a4941ce
Shinibak <felipehenriquelamas@gmail.com>
2022-12-30 22:09:16 -0300

    theme config

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:challenge02_fteam/src/theme/theme_constants.dart';
+import 'package:challenge02_fteam/src/views/home_page.dart';
+import 'package:flutter/material.dart';
+
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: lightTheme,
+      home: const HomePage(),
+    );
+  }
+}
